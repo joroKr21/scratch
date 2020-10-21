ab1241c95fc9ffa070c8a2cd6790ee0b09b03cb4
Georgi Krastev <joro.kr.21@gmail.com>
2020-10-21 04:21:18 +0200

    Add GADT unsoundness test

---
+++ src/main/scala/GadtSoundness.scala
@@ -0,0 +1,24 @@
+object GadtSoundness {
+
+  trait Trait[A]
+
+  class CaseA extends Trait[Int]
+
+  class CaseB extends Trait[Boolean]
+
+  object Pattern {
+    def unapply(fa: CaseB): Option[CaseB] = None
+  }
+
+  def foo[A](t: Trait[A]) = {
+    def bar(f: Trait[A]): Unit = {}
+
+//    t match {
+//      case Pattern(_) =>
+//    }
+
+    t match {
+      case a: CaseA => bar(new CaseA)
+    }
+  }
+}
